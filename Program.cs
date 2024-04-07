4e7ac19b9a21b86a8d740cf308684245ddd9abc4
Alex <Alexey-Idan-151@mail.ru>
2024-04-07 15:35:39 +0300

    Добавлена функция ввода строк

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿class Program
+{
+    public static string[] EnteringLines()
+    {
+        int NumberOfLines = 3;
+        string[] Strging = new string[NumberOfLines];
+        for (int i = 0; i < NumberOfLines;)
+        {
+            Console.WriteLine($"Введите строку №{i + 1}:");
+            Strging[i] = Console.ReadLine()!;
+            Console.WriteLine();
+        }
+
+        return Strging;
+    }
+}
